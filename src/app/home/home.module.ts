05479cd8b0876dff7cb9813a97c371c288ea3d4a
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2024-09-24 13:22:18 -0600

    add: se creo el proyecto. add:se crearon los modulos home, perifericos, computadoras. add:se creo la nav-bar con estilos.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginaComponent } from './pagina/pagina.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaginaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
