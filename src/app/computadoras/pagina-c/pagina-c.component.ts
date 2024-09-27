e0c7197f95436bb3286f510df884a21a1147566b
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2024-09-27 10:46:25 -0600

    add: se agregaron todas las imagenes restantes que faltaban

--- src/app/computadoras/pagina-c/pagina-c.component.ts
+++ src/app/computadoras/pagina-c/pagina-c.component.ts
@@ -7,36 +7,30 @@ import { Component } from '@angular/core';
 })
 export class PaginaCComponent {
 
-  svgIcon1 = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-desktop" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  svgIcon1 = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-receipt-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FF3A3A" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M3 5a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10z" />
-  <path d="M7 20h10" />
-  <path d="M9 16v4" />
-  <path d="M15 16v4" />
+  <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16l-3 -2l-2 2l-2 -2l-2 2l-2 -2l-3 2" />
+  <path d="M14 8h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5m2 0v1.5m0 -9v1.5" />
 </svg>`;
 
-svgIcon2 = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-devices-pc" width="56" height="56" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+svgIcon2 = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-gamepad-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FF3A3A" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M3 5h6v14h-6z" />
-  <path d="M12 9h10v7h-10z" />
-  <path d="M14 19h6" />
-  <path d="M17 16v3" />
-  <path d="M6 13v.01" />
-  <path d="M6 16v.01" />
+  <path d="M12 5h3.5a5 5 0 0 1 0 10h-5.5l-4.015 4.227a2.3 2.3 0 0 1 -3.923 -2.035l1.634 -8.173a5 5 0 0 1 4.904 -4.019h3.4z" />
+  <path d="M14 15l4.07 4.284a2.3 2.3 0 0 0 3.925 -2.023l-1.6 -8.232" />
+  <path d="M8 9v2" />
+  <path d="M7 10h2" />
+  <path d="M14 10h2" />
 </svg>`;
 
-svgIcon3 = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cpu" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+svgIcon3 = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-skyscraper" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FF3A3A" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M5 5m0 1a1 1 0 0 1 1 -1h12a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-12a1 1 0 0 1 -1 -1z" />
-  <path d="M9 9h6v6h-6z" />
-  <path d="M3 10h2" />
-  <path d="M3 14h2" />
-  <path d="M10 3v2" />
-  <path d="M14 3v2" />
-  <path d="M21 10h-2" />
-  <path d="M21 14h-2" />
-  <path d="M14 21v-2" />
-  <path d="M10 21v-2" />
+  <path d="M3 21l18 0" />
+  <path d="M5 21v-14l8 -4v18" />
+  <path d="M19 21v-10l-6 -4" />
+  <path d="M9 9l0 .01" />
+  <path d="M9 12l0 .01" />
+  <path d="M9 15l0 .01" />
+  <path d="M9 18l0 .01" />
 </svg>`;
 
 }
